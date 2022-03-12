160ffd54fff96fa1be42d1199bc7401371c366e2
Scott Tunstall <scott.tunstall@ntlworld.com>
2022-03-12 12:40:41 +0000

    Initial checkin for safekeeping. Runtime: 160 ms, faster than 83.03% of C# online submissions for Two Sum. Memory Usage: 43.3 MB, less than 33.59% of C# online submissions for Two Sum.

---
+++ LeetcodeTwoSum/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using LeetcodeTwoSum;
+
+var solution = new Solution();
+
+int[] answer;
+//var answer = solution.TwoSum(new int[] {2, 7, 11, 15}, 9);
+
+//answer = solution.TwoSum(new int[] {0, 4, 3, 0}, 0);
+
+answer = solution.TwoSum(new int[] {-3, 4, 3, 90}, 0);
+
+Console.ReadLine();
---
+++ LeetcodeTwoSum/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            // maps number to index found in nums array
+            Dictionary<int, int> indexesOfFoundNumbers = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var value = nums[i];
+
+                if (!indexesOfFoundNumbers.ContainsKey(value))
+                    indexesOfFoundNumbers[value] = i;
+
+                var numberWanted = target - value;
+                
+                if (indexesOfFoundNumbers.ContainsKey(numberWanted) && indexesOfFoundNumbers[numberWanted]!=i)
+                {
+                    return new int[] { i, indexesOfFoundNumbers[numberWanted]};
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
